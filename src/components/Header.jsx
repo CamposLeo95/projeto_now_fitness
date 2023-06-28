f123c1ea9fb098a71ca27eb80c3b30b02058867d
CamposLeo95 <leocampos.995@gmail.com>
2023-06-27 22:27:49 -0300

    :sparkles: Criando logica carrinho e troca de produtos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,34 +7,59 @@ import logo from '../assets/img/logo.png'
 import lupa from '../assets/img/icons/lupa.png'
 
 //Hooks
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Header(){
 
-    const [showInput, setShowInput] = useState(false)
+    const [compras, setCompras] = useState(0)
+    const [windowWidth] = useState(window.innerWidth)
+    const [showSearch, setShowSearch] = useState(true)
 
-    const handleInput = () =>{
-        setShowInput(!showInput)
+    const handleSearch = () =>{
+        setShowSearch(!showSearch)
     }
 
+    useEffect(()=>{
+        if(windowWidth < 768){
+            setShowSearch(false)
+        }
+    },[])
+
+    useEffect(()=>{
+        const itens = localStorage.getItem("compra")
+        if(itens === null ){
+            setCompras(0)
+        }else{
+            const qtdCompras = JSON.parse(itens)
+            setCompras(qtdCompras)
+        }
+
+    },[compras])
 
     return(
-        <div className='header'>
+        <header className='header'>
             <img className='logo' src={logo} alt="logo" />
-            <div className='pesquisa'>
-                {showInput 
-                ?<input className='input-pesquisa mobile' type="text" placeholder='Pesquisar' />
-                : <input className='input-pesquisa mobile hidden' type="text" placeholder='Pesquisar' />
-                }
-                <button className='button btn-pesquisa' onClick={() => handleInput()}>
-                    <img className="icons" src={lupa} alt="pesquisa" />
+            <div className='search'>
+            {showSearch &&
+                <input 
+                className='input-search mobile' 
+                type="text" 
+                placeholder="pesquisar..."/>
+            }  
+                <button className='button btn-search'>
+                    <img 
+                        className="icons" 
+                        src={lupa} 
+                        alt="pesquisa" 
+                        onClick={handleSearch}
+                    />
                 </button>
-                <button className='button btn-carrinho'>
-                    <img className='icons carrinho' src={carrinho} alt="carrinho" />
-                </button>
-                
+                <button className='button btn-car'>
+                    <img className='icons' src={carrinho} alt="carrinho" />
+                    {compras !== 0 && <div className='itens_car'>{compras}</div>}
+                </button>         
             </div>
-        </div>
+        </header>
 
     )
 }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,39 +1,97 @@
+//import styles
 import '../styles/components/product.scss'
 
+//import fake database
 import produtos from '../services/produtos.json'
 
+//import images
 import choco from '../assets/img/products/whey_choco.png'
+import suisse from '../assets/img/products/whey_suisse.png'
+import cream from '../assets/img/products/whey_cream.png'
+import { useState } from 'react'
 
 export default function product(){
-    return(
 
-            <div className='main'>
+    const [imageProduct, setImageProduct] = useState(choco)
+    const [titleProduct, setTitleProduct] = useState(produtos.name.chocolatinho)
+
+    const [quantity, setQuantity] = useState(0)
+
+    const handleProduct = (sabor) =>{
+        switch (sabor) {
+            case 'chocolatinho':
+                setImageProduct(choco)
+                setTitleProduct(produtos.name.chocolatinho)
+                break;
+            case 'suisse':
+                setImageProduct(suisse)
+                setTitleProduct(produtos.name.morango)
+                break;
+            case 'cream':
+                setImageProduct(cream)
+                setTitleProduct(produtos.name.cream)
+                break;
+        
+            default:
+                break;
+        }
+    }
+
+    const handleBuy = () =>{
+        const qtd = localStorage.getItem("compra")
+        if(qtd !== null){
+            const qtdLocalStorage= Number(JSON.parse(qtd))
+            const total = Number(qtdLocalStorage) + Number(quantity)
+            console.log(typeof(total))
+            localStorage.setItem("compra", JSON.stringify(total))
+        }else{
+            localStorage.setItem("compra", JSON.stringify(quantity))
+        }
+        
+    }
+
+    return(
+            <main className='main'>
                 <div className='card_image'>
-                    <img className="image_main" src={choco} alt="" />
+                    <img className="image_main" src={imageProduct} alt="" />
                     <div className='controller'>
-                        <button className='btn_controller'>
-                            <img src={choco} alt="" width="50px"/>
+                        <button 
+                            className='btn_controller'
+                            onClick={() => handleProduct('chocolatinho')}
+                        >
+                            <img src={choco} alt="chocolatinho" width="50px"/>
                         </button>
-                        <button className='btn_controller'>
-                            <img src={choco} alt="" width="50px"/>
+                        <button 
+                            className='btn_controller'
+                            onClick={() => handleProduct('suisse')}
+                        >
+                            <img src={suisse} alt="suisse" width="50px"/>
                         </button>
-                        <button className='btn_controller'>
-                            <img src={choco} alt="" width="50px"/>
+                        <button 
+                            className='btn_controller'
+                            onClick={() => handleProduct('cream')}
+                        >
+                            <img src={cream} alt="cream" width="50px"/>
                         </button>
                     </div>
                 </div>
                 <div className='product-info'>
-                    <h1 className='text_main'>{produtos.name.chocolate}</h1>
+                    <h1 className='text_main'>{titleProduct}</h1>
                     <div className='promotion'>
-                    <h2 className='old_price'>R$299,90</h2> 
-                    <h2 className='new_price'>R${produtos.preco}</h2> 
+                        <h2 className='old_price'>de: R$299,90</h2> 
+                        <h2 className='new_price'>por: R${produtos.preco}</h2> 
                     </div>
                     <div className='buy'>
-                        <input className="input_buy" type="number" />  
-                        <button className='btn_buy'>Compre Agora</button>  
+                        <input 
+                            className="input_buy" 
+                            type="number"  
+                            placeholder='Qtd.'
+                            onChange={(e) => setQuantity(e.target.value)}
+                            />  
+                        <button className='btn_buy' onClick={handleBuy}>Compre Agora</button>  
                     </div>  
                     <p>{produtos.name.chocolate}</p>
                 </div>
-            </div>
+            </main>
     )
 }
