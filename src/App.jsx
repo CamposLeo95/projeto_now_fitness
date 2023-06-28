98d534fb6a4af98d82c719edd997c3d633dfb952
CamposLeo95 <leocampos.995@gmail.com>
2023-06-27 22:26:24 -0300

    :sparkles: Criando container e add components ao app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import Description from "./components/Description"
 import Header from "./components/Header"
 import Product from "./components/Product"
+import Footer from "./components/footer"
+import './styles/app.scss'
 
 function App () {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Product />
+      <Description />
+      <Footer />
     </div>
   )
 }
